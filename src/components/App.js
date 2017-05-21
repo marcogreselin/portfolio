a9baf78ea42fca3093ebf5568b80de8aa82e9277
Marco <marcogreselin@me.com>
2017-05-21 18:56:28 +0200

    two pages mobile tested!

--- src/components/App.js
+++ src/components/App.js
@@ -9,16 +9,23 @@ class App extends Component {
   render() {    
     return (
       <div className="App">
-
-        <div className="centered-wrapper">
-          <Switch>
-            <Route exact path="/" component={Welcome} />
-            <Route exact path="/life" component={Life} />
-            <Route path="*" component={Welcome}/>
-          </Switch>
-
-        </div>                
-        { (this.props.location.pathname !== "/life") && (<div className="who">[ <Link to="/life">Who's Marco</Link> ]</div>)}
+          <div className="content">
+          <div className="centered-wrapper">
+            <Switch>
+              <Route exact path="/" component={Welcome} />
+              <Route exact path="/life" component={Life} />
+              <Route path="*" component={Welcome}/>
+            </Switch>
+          </div>                
+          { (this.props.location.pathname !== "/life") && (<div className="who">[ <Link to="/life">Who's Marco</Link> ]</div>)}
+          { (this.props.location.pathname === "/life") && (<div className="back">[ <Link to="/">Go Back Home</Link> ]</div>)}
+        </div>
+        <footer>
+          <a href="https://www.github.com/marcogreselin" target="_blank" className="social" rel="noopener noreferrer">G.H.</a>
+          <a href="https://www.linkedin.com/in/marcogreselin/" target="_blank" className="social" rel="noopener noreferrer">L.I.</a>
+          <a href="mailto:marcogreselin@me.com?subject=Let&#8217;s get a coffee!" className="social" rel="noopener noreferrer">E.M.</a>
+          <a href="https://www.instagram.com/marcogreselin" target="_blank" className="social" rel="noopener noreferrer">I.G.</a>
+        </footer>
       </div>
     );
   }
--- src/components/Life.js
+++ src/components/Life.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import '../styles/Life.css'
 import profilePic from '../images/marco.jpg'
-import { Link } from 'react-router-dom'
 // import App from './App'
 
 const Life = () => (
     <div className="Life">
-        <img src={profilePic} alt="Profile"/>
-        <div className="story">
-            <Link to="/">back</Link>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <div className="title">Make. Break. Repeat.</div>
+        <div className="image-description">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=Il3RtJ1DfE4"><img src={profilePic} alt="Profile"/></a>
+            <div className="story">
+                <p>Ciao! I’m Marco and I love to build products that help people in their everyday lives. They call this the art of Product Management in the rehalm of tech companies.</p> 
+                <p>Coding used to be a hobby when I was studying Economics in undergraduate and became my primary occupation when I converted to CS at <a target="_blank" rel="noopener noreferrer" href="http://www.ucl.ac.uk/">UCL</a>. 
+                So while I look for a new hobby, I spend my time <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/marcogreselin/videos/vb.1424364048/10211475287784227/">traveling</a>, finding <a target="_blank" rel="noopener noreferrer" href="http://99u.com/articles">inspirations</a> and (design) <a  target="_blank" rel="noopener noreferrer" href="https://vimeo.com/5189134">thinking</a>.</p>
+                <p>Oh, and like any good Italian I also love coffee. So if you are in London, <a href="mailto:marcogreselin@me.com?subject=Let&#8217;s get a coffee!">email</a> me and let's meet!</p>
+                <div className="row">
+                    <div className="two take">
+                    <div className="title">Some of the people I worked with</div>
+                    <div className="list">
+                        <ul>
+                            <li>Jan - May 17: Resident @ AKQA</li>
+                            <li>Sept - Dec 16: Resident @ EF</li>
+                            <li>Jun - Sept 15: Intern @ YouTube</li>
+                            <li>Feb 14 - Feb 15: BI @ Rocket Internet</li>
+                            <li>Jul - Aug 13: Intern @ Skype</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="two receive">
+                    <div className="title">Contacts</div>
+                    <div className="list">
+                         <ul>
+                            <li><a href="mailto:marcogreselin@me.com?subject=Let&#8217;s get a coffee!">marcogreselin@me.com</a></li>
+                            <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/marcogreselin/">linkedin.com/in/marcogreselin</a></li>
+                        </ul>
+                    </div>
+                </div>
+                </div>
+                
+            </div>
         </div>
     </div>
 )
